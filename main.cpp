82a7e9cf673c295fcac6ec38e56db89091238cab
luisejv <ljauregui@pucp.pe>
2020-04-06 17:44:12 -0500

    cant run it

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
-int main(){
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    long long int firstNumber, secondNumber;
+    double root;
+    do
+    {
+        cin >> firstNumber;
+        cin >> secondNumber;
+        root = pow(secondNumber, 1 / firstNumber);
+        cout << root << endl;
+    } while (firstNumber != 0 && secondNumber != 0);
+    return 0;
 }
